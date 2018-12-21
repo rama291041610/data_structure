75b0ab81889d86fe79a6afcb44ae366d63f193c1
惊蛰 <jingzhe@live.cn>
2018-12-21 08:57:43 +0800

    Update graph.cpp

--- Lab/Lab3/graph.cpp
+++ Lab/Lab3/graph.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int n;
 int mat[N][N],dp[N][N],path[N][N];
+int vis[N];
 
 map <int,string> dict;
 
@@ -30,10 +31,22 @@ void print_path(int i,int j){
         }
 }
 
-void file_input(){
+void dfs(int cur){
+        vis[cur]=1;
+        cout<<dict[cur]<<" ";
+        for(int i=0; i<n; i++) {
+                if(mat[cur][i]!=-1&&!vis[i]) {
+                        dfs(i);
+                        vis[i]=0;
+                }
+        }
+}
+
+void init(){
         memset(path,0,sizeof(path));
         memset(mat,0,sizeof(mat));
         memset(dp,0,sizeof(dp));
+        memset(vis,0,sizeof(vis));
 
         scanf("nodeNum:%d",&n);
         string node;
@@ -59,22 +72,26 @@ int main() {
         freopen("./ex3_Graph1.txt","r",stdin);
 
         cout<<"Graph1:"<<endl;
-        file_input();
+        init();
         floyd();
         for(int i=1; i<n; i++) {
                 cout<<"\ts0->"<<dict[i]<<" path: s0,";
                 print_path(0,i);
                 cout<<"\b"<<"\t"<<"value="<<dp[0][i]<<endl;
         }
-        cout<<endl;
+        dfs(0);
+        cout<<"\n"<<endl;
+
 
         freopen("./ex3_Graph2.txt","r",stdin);
 
         cout<<"Graph2:"<<endl;
-        file_input();
+        init();
         floyd();
         cout<<"\ta->"<<dict[7]<<" path: a,";
         print_path(0,7);
         cout<<"\b"<<"\t"<<"value="<<dp[0][7]<<endl;
+        dfs(0);
+        cout<<endl;
         return 0;
 }
