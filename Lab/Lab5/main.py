ea644e97bbf5a00a93da7bb63997646e8c817f8b
惊蛰 <jingzhe@live.cn>
2018-12-27 22:50:22 +0800

    update lab5

--- Lab/Lab5/main.py
+++ Lab/Lab5/main.py
@@ -19,6 +19,10 @@ def generate_data(n, method="order"):
     return data
 
 
+def average(x):
+    return sum(x) / len(x)
+
+
 def analize(N, data, method):
     map = {'order': u"顺序", 'anti': u"逆序", 'rand': u"随机"}
 
@@ -33,10 +37,10 @@ def analize(N, data, method):
     y2 = np.array(data['quick_sort'])
 
     x_new = np.linspace(x.min(), x.max(), 10000)
-    f = interpolate.interp1d(x, y1, 2)
+    f = interpolate.UnivariateSpline(x, y1, k=2, s=1)
     plt.plot(x_new, f(x_new), label=u"冒泡排序")
 
-    f = interpolate.UnivariateSpline(x, y2, s=0.005)
+    f = interpolate.UnivariateSpline(x, y2, k=2, s=1)
     plt.plot(x_new, f(x_new), c='c', label=u"快速排序")
 
     plt.legend()
@@ -44,7 +48,7 @@ def analize(N, data, method):
 
 
 if __name__ == "__main__":
-    N = [10, 50, 100, 300, 500, 1000]
+    N = [10, 50, 100, 300, 500, 800, 1500, 2500]
     if os.path.exists("time_use.data"):
         time_use = pickle.load(open("time_use.data", "rb"))
     else:
@@ -52,17 +56,21 @@ if __name__ == "__main__":
                     'quick_sort': {'order': [], 'anti': [], 'rand': []}}
         for n in N:
             for method in ['order', 'anti', 'rand']:
-                data = generate_data(n, method)
-                data_cp = data
-                start_time = time.clock()
-                buddle_sort(data)
-                end_time = time.clock()
+                time_tmp = {'quick_sort': [], 'buddle_sort': []}
+                for i in range(10):
+                    data = generate_data(n, method)
+                    data_cp = data
+                    start_time = time.clock()
+                    buddle_sort(data)
+                    end_time = time.clock()
+                    time_tmp['buddle_sort'].append((end_time - start_time) * 1e3)
+                    start_time = time.clock()
+                    quick_sort(data_cp, 0, len(data_cp) - 1)
+                    end_time = time.clock()
+                    time_tmp['quick_sort'].append((end_time - start_time) * 1e3)
 
-                time_use['buddle_sort'][method].append((end_time - start_time) * 1e3)
-                start_time = time.clock()
-                quick_sort(data_cp, 0, len(data_cp) - 1)
-                end_time = time.clock()
-                time_use['quick_sort'][method].append((end_time - start_time) * 1e3)
+                time_use['buddle_sort'][method].append(average(time_tmp['buddle_sort']))
+                time_use['quick_sort'][method].append(average(time_tmp['quick_sort']))
         pickle.dump(time_use, open("time_use.data", "wb"))
 
     for method in ['order', 'anti', 'rand']:
